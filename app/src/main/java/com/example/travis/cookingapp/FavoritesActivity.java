8f6aec7d27956de0025068057fff0b277f5cfa12
Connor Smith <css13c@acu.edu>
2017-12-12 18:10:09 -0600

    Got favorites workinggit pull

--- app/src/main/java/com/example/travis/cookingapp/FavoritesActivity.java
+++ app/src/main/java/com/example/travis/cookingapp/FavoritesActivity.java
@@ -1,15 +1,22 @@
 package com.example.travis.cookingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.travis.cookingapp.database.DataSource;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FavoritesActivity extends AppCompatActivity {
     DataSource mDataSource;
     public  List<FoodResult> Favorites;
+    private RecyclerView mResultsView;
+    private FoodAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,24 @@ public class FavoritesActivity extends AppCompatActivity {
         mDataSource.open();
 
         Favorites = mDataSource.getAllItems();
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Set-up results list
+        mResultsView = (RecyclerView) findViewById(R.id.resultsList);
+        mResultsView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mResultsView.setLayoutManager(mLayoutManager);
+
+        // Set-up results list adapter
+        mAdapter = new FoodAdapter(this, new ArrayList<FoodResult>(0), new FoodAdapter.PostItemListener() {
+            @Override
+            public void onPostClick(String href) {
+                Intent webIntent = new Intent(getApplicationContext(), WebActivity.class);
+                webIntent.putExtra("href", href);
+                startActivity(webIntent);
+            }
+        });
+        mResultsView.setAdapter(mAdapter);
+        mAdapter.updateResults(Favorites);
     }
 }
